276041bce6cba12dedfaa5b7516a5107fbdc648f
naimul64 <naimul64@yahoo.com>
2015-05-08 12:37:03 +0600

    c3

--- application/controllers/addcard.php
+++ application/controllers/addcard.php
@@ -9,5 +9,11 @@ class AddCard extends CI_Controller {
 
 	function addCard() {
 		$card_id=$this->input->post('CardID');
+		$pan=$this->input->post('PAN');
+		$debitorcredit=$this->input->post('DebitOrCredit');
+		$visaormaster=$this->input->post('VisaOrMaster');
+		$expdate=$this->input->post('ExpDate');
+		$cardlimit=$this->input->post('CardLimit');
+		$vercode=$this->input->post('VerificationCode');
 	}
 }
--- application/models/card.php
+++ application/models/card.php
@@ -1,6 +1,16 @@
 <?php
 class Card extends CI_Model {
-	static function addCard() {
-		
+	static function addCard($cardid,$pan,$debitorcredit,$visaormaster,$expdate,$cardlimit,$vercode) {
+		$data = array(
+			'CardID' => $cardid,
+			'PAN' => $pan,
+			'DebitOrCredit' => $debitorcredit,
+			'VisaOrMaster' => $visaormaster,
+			'ExpDate' => $expdate,
+			'CardLimit' => $cardlimit,
+			'VerificationCode' => $vercode,
+		);
+		$this->db->insert('Card', $data);
 	}
+	
 }
