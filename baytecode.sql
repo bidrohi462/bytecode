c69fc24fd15a63b82f59fec5b31a264dce1373ac
bidrohi462 <shovan.livebd@yahoo.com>
2015-05-08 10:57:07 +0600

    database schema, erd

---
+++ baytecode.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2015 at 06:53 AM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `baytecode`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE IF NOT EXISTS `account` (
+  `Account_no` int(20) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(25) NOT NULL,
+  `Phone_no` varchar(15) NOT NULL,
+  `Address` text NOT NULL,
+  `Balance` int(11) NOT NULL,
+  PRIMARY KEY (`Account_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `card`
+--
+
+CREATE TABLE IF NOT EXISTS `card` (
+  `CardID` int(25) NOT NULL AUTO_INCREMENT,
+  `PAN` int(25) NOT NULL,
+  `Type1` varchar(15) NOT NULL,
+  `Type2` varchar(15) NOT NULL,
+  `ExpDate` date NOT NULL,
+  `CardLimit` int(5) NOT NULL,
+  `VerificationCode` varchar(30) NOT NULL,
+  PRIMARY KEY (`CardID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deposit`
+--
+
+CREATE TABLE IF NOT EXISTS `deposit` (
+  `ReceiptNo` int(12) NOT NULL,
+  `Amount` int(15) NOT NULL,
+  `BranchID` int(8) NOT NULL,
+  `PAN` int(25) NOT NULL,
+  PRIMARY KEY (`ReceiptNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `officer`
+--
+
+CREATE TABLE IF NOT EXISTS `officer` (
+  `Officer Id` varchar(15) NOT NULL,
+  `Password` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE IF NOT EXISTS `transaction` (
+  `TransID` int(30) NOT NULL AUTO_INCREMENT,
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `MachineID` int(25) NOT NULL,
+  `PAN` int(25) NOT NULL,
+  `Success` tinyint(1) NOT NULL,
+  `CardID` int(25) NOT NULL,
+  `Amount` int(8) NOT NULL,
+  PRIMARY KEY (`TransID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
